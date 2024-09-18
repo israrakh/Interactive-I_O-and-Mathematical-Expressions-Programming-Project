b0e5ebe826bd118d9a1709d0b9de1fd575d561de
Israr <israr.akh03@gmail.com>
2024-09-18 19:28:33 -0400

    Add project files.

---
+++ Interactive I_O and Mathematical Expressions Programming Project.cpp
@@ -0,0 +1,9 @@
+// Interactive I_O and Mathematical Expressions Programming Project.cpp : This project covers two programming examples, Ingredient Adjuster and Interest Earned.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "This project covers two programming examples, Ingredient Adjuster and Interest Earned.\n";
+}
+
